b9683df8af70ade252aa6b9ee7be83d62eed5764
Aleksandra Wyporkiewicz <80450315+wyola@users.noreply.github.com>
2025-03-05 10:01:30 +0100

    orgs table pagination

---
+++ frontend/src/components/CustomPagination/CustomPagination.tsx
@@ -0,0 +1,58 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import './customPagination.scss';
+
+type CustomPaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+};
+
+export const CustomPagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: CustomPaginationProps) => {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  //workaround for disabling prev/next buttons (no "disabled" prop in PaginationNext/PaginationPrevious)
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  return (
+    <Pagination className="pagination">
+      <PaginationContent>
+        <PaginationItem className="pagination__button">
+          <PaginationPrevious
+            onClick={() => !isFirstPage && onPageChange(currentPage - 1)}
+            className={isFirstPage ? 'pagination__disabled' : ''}
+          />
+        </PaginationItem>
+
+        {pages.map((page) => (
+          <PaginationItem key={page} className="pagination__button">
+            <PaginationLink
+              onClick={() => onPageChange(page)}
+              isActive={page === currentPage}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        <PaginationItem className="pagination__button">
+          <PaginationNext
+            onClick={() => !isLastPage && onPageChange(currentPage + 1)}
+            className={isLastPage ? 'pagination__disabled' : ''}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
