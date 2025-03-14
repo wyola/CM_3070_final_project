89209dbea6f4c8f73a5c560e1f5675ed8eac9455
Aleksandra Wyporkiewicz <80450315+wyola@users.noreply.github.com>
2025-03-14 10:28:40 +0100

    Extract add needs section (#35)
    * extract needs section to component
    
    * move styles
    
    * component for when no needs are added

---
+++ frontend/src/components/OrganizationsNeeds/OrganizationsNeeds.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import { AddNeedModal, Button, CustomCard, Need } from '@/components';
+import { API_ENDPOINTS } from '@/constants';
+import axios from 'axios';
+import { axiosInstance } from '@/lib/axios';
+import { KindsOfNeeds, NeedI } from '@/types';
+import './organizationNeeds.scss';
+
+type OrganizationNeedsProps = {
+  organizationId: number;
+};
+
+export const OrganizationsNeeds = ({
+  organizationId,
+}: OrganizationNeedsProps) => {
+  const [error, setError] = useState<string | null>(null);
+  const [needs, setNeeds] = useState<NeedI[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchNeeds = async () => {
+    try {
+      const { data } = await axiosInstance.get(
+        API_ENDPOINTS.NEEDS.ALL(organizationId)
+      );
+      setNeeds(data.needs);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data?.message || 'Failed to load needs');
+      } else {
+        setError('An unexpected error occurred');
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchOrganization = async () => {
+      try {
+        const needs = await axiosInstance.get(
+          API_ENDPOINTS.NEEDS.ALL(organizationId)
+        );
+        setNeeds(needs.data.needs);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.response?.data?.message || 'Failed to load needs');
+        } else {
+          setError('An unexpected error occurred');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchOrganization();
+  }, [organizationId]);
+
+  return (
+    <div className="needs">
+      <div className="needs__header">
+        <h2 className="heading-secondary">Current needs</h2>
+        <Button
+          variant="link"
+          className="needs__add-button"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <img src="/add.svg" alt="add new need" />
+        </Button>
+      </div>
+      {needs.length > 0 ? (
+        needs.map((need) => (
+          <Need
+            key={need.id}
+            id={need.id}
+            kind={need.kind as KindsOfNeeds}
+            priority={need.priority}
+            description={need.description}
+          />
+        ))
+      ) : (
+        <CustomCard className="needs__empty">
+          <p>No needs posted yet. Add first need using add button above!</p>
+        </CustomCard>
+      )}
+
+      <AddNeedModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        organizationId={organizationId}
+        onSuccess={fetchNeeds}
+      />
+    </div>
+  );
+};
--- frontend/src/routes/Organization/Organization.tsx
+++ frontend/src/routes/Organization/Organization.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import {
-  Button,
   CustomCard,
   OrganizationContact,
   OrganizationHeader,
   OrganizationMap,
-  OrganizationsNeed,
-  AddNeedModal,
+  OrganizationsNeeds,
 } from '@/components';
-import { OrganizationI, KindsOfNeeds, NeedI } from '@/types';
+import { OrganizationI } from '@/types';
 import { axiosInstance } from '@/lib/axios';
 import axios from 'axios';
 import { API_ENDPOINTS } from '@/constants';
@@ -17,40 +15,18 @@ import './organization.scss';
 
 export const Organization = () => {
   const [organization, setOrganization] = useState<OrganizationI | null>(null);
-  const [needs, setNeeds] = useState<NeedI[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { id } = useParams();
-
-  // Needed to display add/edit buttons only for owner
-  // const isOwner = true; // TODO: add to organization ownerId and check if user is owner
-
-  const fetchNeeds = async () => {
-    try {
-      const { data } = await axiosInstance.get(
-        API_ENDPOINTS.NEEDS.ALL(Number(id))
-      );
-      setNeeds(data.needs);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        setError(error.response?.data?.message || 'Failed to load needs');
-      } else {
-        setError('An unexpected error occurred');
-      }
-    }
-  };
 
   useEffect(() => {
     const fetchOrganization = async () => {
       try {
-        const [orgResponse, needsResponse] = await Promise.all([
-          axiosInstance.get(API_ENDPOINTS.ORGANIZATIONS.BY_ID(Number(id))),
-          axiosInstance.get(API_ENDPOINTS.NEEDS.ALL(Number(id))),
-        ]);
+        const orgResponse = await axiosInstance.get(
+          API_ENDPOINTS.ORGANIZATIONS.BY_ID(Number(id))
+        );
         setOrganization(orgResponse.data.organization);
-        setNeeds(needsResponse.data.needs);
       } catch (error) {
         if (axios.isAxiosError(error)) {
           setError(
@@ -107,39 +83,12 @@ export const Organization = () => {
         />
       )}
 
-      <div className="organization__needs">
-        <div className="organization__needs--header">
-          <h2 className="heading-secondary">Current needs</h2>
-          <Button
-            variant="link"
-            className="organization__needs--add-button"
-            onClick={() => setIsModalOpen(true)}
-          >
-            <img src="/add.svg" alt="add new need" />
-          </Button>
-        </div>
-        {needs.map((need) => (
-          <OrganizationsNeed
-            key={need.id}
-            id={need.id}
-            kind={need.kind as KindsOfNeeds}
-            priority={need.priority}
-            description={need.description}
-          />
-        ))}
-      </div>
+      <OrganizationsNeeds organizationId={Number(id)} />
 
       <div className="organization__volunteering">
         <h2 className="heading-secondary">Volunteering options</h2>
         <CustomCard>volunteering options</CustomCard>
       </div>
-
-      <AddNeedModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        organizationId={Number(id)}
-        onSuccess={fetchNeeds}
-      />
     </section>
   );
 };
